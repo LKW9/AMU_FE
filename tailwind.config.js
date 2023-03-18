a5c7937601e8dc85ba99df929b3ef4cd1a834d20
ramigou <dasul1021@gmail.com>
2023-03-18 10:47:51 +0900

    feat: modify tailwind, postcss, vite config files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./*.html', './src/**/*.{vue,js,ts,jsx,tsx,css}'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
