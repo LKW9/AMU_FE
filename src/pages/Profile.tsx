71f4f426a0e5dc66abde944f3acf28d237187142
ramigou <dasul1021@gmail.com>
2023-03-24 19:07:03 +0900

    refactor: dark mode class 제거

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -108,7 +108,7 @@ export default function Profile() {
               <div className="flex items-center justify-center w-full">
                 <label
                   htmlFor="dropzone-file"
-                  className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                  className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 "
                 >
                   <div className="flex flex-col items-center justify-center pt-5 pb-6">
                     {preview ? (
@@ -130,11 +130,11 @@ export default function Profile() {
                             d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
                           ></path>
                         </svg>
-                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                        <p className="mb-2 text-sm text-gray-500">
                           <span className="font-semibold">Click to upload</span>{' '}
                           or drag and drop
                         </p>
-                        <p className="text-xs text-gray-500 dark:text-gray-400">
+                        <p className="text-xs text-gray-500">
                           SVG, PNG, JPG or GIF (MAX. 800x400px)
                         </p>
                       </>
@@ -223,10 +223,10 @@ export default function Profile() {
         className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full"
       >
         <div className="relative p-4 w-full max-w-md h-full md:h-auto">
-          <div className="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+          <div className="relative p-4 text-center bg-white rounded-lg shadow">
             <button
               type="button"
-              className="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              className="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
               data-modal-toggle="deleteModal"
             >
               <svg
@@ -245,7 +245,7 @@ export default function Profile() {
               <span className="sr-only">Close modal</span>
             </button>
             <svg
-              className="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto"
+              className="text-gray-400 w-11 h-11 mb-3.5 mx-auto"
               aria-hidden="true"
               fill="currentColor"
               viewBox="0 0 20 20"
@@ -257,20 +257,20 @@ export default function Profile() {
                 clip-rule="evenodd"
               ></path>
             </svg>
-            <p className="mb-4 text-gray-500 dark:text-gray-300">
+            <p className="mb-4 text-gray-500">
               Are you sure you want to delete this item?
             </p>
             <div className="flex justify-center items-center space-x-4">
               <button
                 data-modal-toggle="deleteModal"
                 type="button"
-                className="py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                className="py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10"
               >
                 No, cancel
               </button>
               <button
                 type="button"
-                className="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900"
+                className="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300"
               >
                 Yes, I'm sure
               </button>
