4eda80bbbbd375c2235fa66934a697bc7eab4f48
한창윤 <hansyooni11@gmail.com>
2023-03-24 23:10:25 +0900

    edit vite.config.ts
    edit vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://backend:3000",
+        target: "http://amu:3000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
